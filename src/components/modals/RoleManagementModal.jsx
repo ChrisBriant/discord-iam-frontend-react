9bd39a02b315b636361833df095db4db68afe971
cbriant <cbriant@btinternet.com>
2026-08-13 05:17:25 +0100

    Added a method which extracts paginated data into one list

--- src/components/modals/RoleManagementModal.jsx
+++ src/components/modals/RoleManagementModal.jsx
@@ -1,9 +1,26 @@
+import { useEffect } from "react";
+import { getUsers, getPagedDataAsList } from "../../network/discord";
+import { useContext } from "react";
+import { Context as DataContext } from "../../context/DataContext";
 
-// import { useContext } from "react";
-// import { Context as DataContext } from "../../context/DataContext";
 
 const RoleManagementModal = ({selectedRole}) => {
-    //const {state:{selectedRole}} = useContext(DataContext);
+    const {state:{users}, setUsers} = useContext(DataContext);
+
+    useEffect(() => {
+        //Load the users
+        // getUsers().then((response) => {
+        //     console.log("RETRIEVED USERS", response);
+        // }).catch((err) => {
+        //     console.error("Unable to retrieve the users", err);
+        // });
+        getPagedDataAsList("/authorisation/users?page_size=3").then((response) => {
+            console.log("RETRIEVED USERS", response);
+            setUsers(response);
+        }).catch((err) => {
+            console.error("Unable to retrieve the users", err);
+        });
+    },[]);
 
     return(
         <div className="calendar-modal-overlay">
@@ -15,6 +32,14 @@ const RoleManagementModal = ({selectedRole}) => {
                     </div>
                     : null
                 }
+                {/* TODO : Need to create a paginated component that pages a list of items */}
+                <div>
+                    {
+                        users.map((u) => (
+                            <p>{u.user_name}</p>
+                        ))
+                    }
+                </div>
             </div>
         </div>
     )
--- src/network/discord.js
+++ src/network/discord.js
@@ -57,6 +57,22 @@ function getRoles() {
     });
 } 
 
+
+function getUsers() {
+    return new Promise( async (resolve,reject) => {
+        const url = `/authorisation/users?page_size=3`;
+
+        conn.get(url)
+        .then( (response) => {
+            return resolve(response.data);
+        }).catch((err) => {
+            console.error("ERROR REJECT",err);
+            return reject(err);
+        });
+    });
+} 
+
+
 function activateRole(roleId) {
     return new Promise( async (resolve,reject) => {
         const url = `/authorisation/${roleId}/activaterole`;
@@ -100,12 +116,46 @@ function goToPage(objectType, page, pageSize=10) {
 }
 
 
+function getPagedDataAsList(url) {
+    return new Promise( async (resolve,reject) => {
+        let itemsList = [];
+        conn.get(url)
+        .then( async (response) => {
+            console.log("NEXT PAGE", response.data.next_page);
+            let nextPage = response.data.next_page;
+            itemsList = response.data.data;
+            while(nextPage) {
+                // conn.get(response.data.next_page).then(nextRes => {
+                //     itemsList = [...itemsList,response.data.data];
+                // }).catch(err => {
+                //     console.error("ERROR REJECT",err);
+                //     return reject(err);
+                // }); 
+                try {
+                    const nextRes = await conn.get(nextPage)
+                    nextPage = nextRes.next_page;
+                    itemsList = [...itemsList,...nextRes.data.data];
+                } catch(err) {
+                    console.error("ERROR REJECT",err);
+                    return reject(err);
+                }; 
+            }
+            return resolve(itemsList);
+        }).catch((err) => {
+            console.error("ERROR REJECT",err);
+            return reject(err);
+        });
+    });
+} 
+
 export { 
     getChannels, 
     getFeed, 
     getEvents, 
     activateRole, 
     deactivateRole, 
-    getRoles, 
-    goToPage
+    getRoles,
+    getUsers,
+    goToPage,
+    getPagedDataAsList,
 };
