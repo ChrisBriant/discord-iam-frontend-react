d5b0a6ad7c1af355c770d9ca53bba55e0ea0202c
cbriant <cbriant@btinternet.com>
2026-09-17 04:10:09 +0100

    Added the users panel to the dashboard

--- src/components/dashPanels/UsersPanel.jsx
+++ src/components/dashPanels/UsersPanel.jsx
@@ -1,18 +1,57 @@
-import { Context as DataContext } from "../../context/AuthContext";
-import { useContext } from "react";
+import { Context as DataContext } from "../../context/DataContext";
+import { useContext, useEffect, useState } from "react";
+import { getPagedDataAsList } from "../../network/discord";
+import ClientPaginatedList from "../UI/ClientPaginatedList";
 
 const UsersPanel = () => {
-    const {state:{users}} = useContext(DataContext);
+    const {state:{users,events},setUsers} = useContext(DataContext);
+    const [selectedUser,setSelectedUser] = useState(null);
+
+
+    const UserCard = ({ item }) => {
+        return(
+            <div 
+                className={ selectedUser?.id === item.id ?  "item userItem selected" : "item userItem" }
+                onClick={() =>  {
+                    if (item && (new Date(fromDate) < new Date(toDate))) setDisableAssign(false);
+                    setSelectedUser(item);
+                }}
+            >
+                <p>{item.user_name}</p>
+            </div>
+        )
+    }
+    
+    useEffect(() => {
+        //Get the users
+        getPagedDataAsList("/authorisation/users").then((response) => {
+            console.log("RETRIEVED USERS", response);
+            setUsers(response);
+            setLoaded(false);
+        }).catch((err) => {
+            console.error("Unable to retrieve the users", err);
+        });
+    },[]);
+
+    console.log("MOUNTED USERS PANEL", users, events);
 
     return (
         <div id="userPanel" className="panel">
-            {
+            {/* {
                 users.map((user) => (
                     <div className="userItem">
                         <p>{user.userName}</p>
                     </div>
                 ))
-            }
+            } */}
+
+            <ClientPaginatedList 
+                items={users}
+                pageSize={2}
+                itemComponent={UserCard}
+                className="paginator"
+                gridClassName="itemSelector"
+            />
 
         </div>
     );
